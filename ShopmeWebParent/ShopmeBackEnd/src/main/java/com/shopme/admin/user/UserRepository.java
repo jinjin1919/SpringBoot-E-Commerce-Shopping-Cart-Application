1ab7eb5f1741dec564ef58c5365f18da5e597c51
jinjin1919 <75953443+jinjin1919@users.noreply.github.com>
2024-01-13 21:26:54 -0500

    add filter

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
@@ -1,5 +1,7 @@
 package com.shopme.admin.user;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -18,6 +20,9 @@ public interface UserRepository extends CrudRepository<User, Integer>, PagingAnd
 	@Query("UPDATE User u SET u.enabled = ?2 WHERE u.id = ?1")
 	@Modifying
 	public void updateEnabledStatus(Integer id, boolean enabled);
+	
+	@Query("SELECT u FROM User u WHERE u.firstName LIKE %?1% OR u.lastName LIKE %?1%")
+	public Page<User> findAll(String keyword, Pageable pageable); 
 
 //	public User save(User userNamHM);
 }
