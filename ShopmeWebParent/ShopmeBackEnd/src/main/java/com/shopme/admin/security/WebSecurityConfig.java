c857392233f245d44343b9faee8bb6d70f6495f0
jinjin1919 <75953443+jinjin1919@users.noreply.github.com>
2024-01-25 22:21:42 -0500

    commit

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -4,11 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class WebSecurityConfig {
 	
 	@Bean
@@ -20,14 +20,31 @@ public class WebSecurityConfig {
 	
 	
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain configureHttpSecurity(HttpSecurity http) throws Exception {
+		
 		
 		http.authorizeHttpRequests((authz) -> authz
-				.anyRequest().permitAll()); 
+				
+				.anyRequest().authenticated()
+				
+				).formLogin( form -> form 
+						.loginPage("/login")
+						.usernameParameter("email")
+						.permitAll()); 
+				
+				
+					
+				
 		
 		return http.build(); 
 		
 		
+	}
+	
+	@Bean
+	WebSecurityCustomizer configureWebSecurity() throws Exception {
+		
+		return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**"); 
 	}
 
 }
