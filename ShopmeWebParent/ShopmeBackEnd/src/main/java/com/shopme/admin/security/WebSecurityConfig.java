2f9334aa71bc9861aae5587d1781e38975934ec2
jinjin1919 <75953443+jinjin1919@users.noreply.github.com>
2024-01-26 14:21:32 -0500

    spring security

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -2,26 +2,44 @@ package com.shopme.admin.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 public class WebSecurityConfig {
 	
 	@Bean
-	public BCryptPasswordEncoder PasswordEncoder() {
+	public UserDetailsService userDetailsService() {
+		return new ShopmeUserDetailsService(); 
+	}
+	
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
 		
 		return new BCryptPasswordEncoder(); 
 	}
+	
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(); 
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+		
+		return authProvider; 
+	}
+	
 	
 	
 	
 	@Bean
 	public SecurityFilterChain configureHttpSecurity(HttpSecurity http) throws Exception {
 		
+		http.authenticationProvider(authenticationProvider()); 
 		
 		http.authorizeHttpRequests((authz) -> authz
 				
