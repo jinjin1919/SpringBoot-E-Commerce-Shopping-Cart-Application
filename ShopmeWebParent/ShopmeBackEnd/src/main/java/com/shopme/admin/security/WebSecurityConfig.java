d70bfcf98b9a8ca6f3344dba802d9efe8b5e5233
jinjin1919 <75953443+jinjin1919@users.noreply.github.com>
2024-01-27 20:20:37 -0500

    logout

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -48,7 +48,9 @@ public class WebSecurityConfig {
 				).formLogin( form -> form 
 						.loginPage("/login")
 						.usernameParameter("email")
-						.permitAll()); 
+						.permitAll())
+				.logout(logout -> logout.permitAll()); 
+		
 				
 				
 					
