fed52df0bf7e37d37dae2097b0cbe665066e5dd1
jinjin1919 <75953443+jinjin1919@users.noreply.github.com>
2024-02-12 12:32:52 -0500

    category

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -32,12 +33,34 @@ public class Category {
 	private String image; 
 	private boolean enabled; 
 	
-	@OneToOne
+	@ManyToOne
 	@JoinColumn(name = "parent_id")
 	private Category parent; 
 	
 	@OneToMany(mappedBy = "parent")
 	private Set<Category> children = new HashSet<>();
+
+	public Category() {
+		
+	}
+	
+	public Category(Integer id) {
+	
+		this.id = id;
+	}
+
+	public Category(String name) {
+	
+		this.name = name;
+		this.alias = name; 
+		this.image = "default.png";
+	}
+	
+	public Category(String name, Category parent) {
+		
+		this(name); 
+		this.parent = parent; 
+	}
 
 	public Integer getId() {
 		return id;
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryRepository.java
@@ -1,9 +1,11 @@
 package com.shopme.admin.category;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.shopme.common.entity.Category;
+import com.shopme.common.entity.User;
 
-public interface CategoryRepository extends PagingAndSortingRepository<Category, Integer> {
+public interface CategoryRepository extends CrudRepository<Category, Integer>, PagingAndSortingRepository<Category, Integer> {
 
 }
