1b6d902747e1ec5cd52956c8bc7138bf7e504eaa
jinjin1919 <75953443+jinjin1919@users.noreply.github.com>
2024-05-04 22:40:57 -0400

    added pagination

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
@@ -1,12 +1,20 @@
 package com.shopme.admin.brand;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.shopme.common.entity.Brand;
+import com.shopme.common.entity.Category;
 
 public interface BrandRepository extends CrudRepository<Brand, Integer>, PagingAndSortingRepository<Brand, Integer> {
 
+	
+	@Query("SELECT b FROM Brand b WHERE b.name LIKE %?1%")
+	public Page<Brand> search(String keyword, Pageable pageable); 
+	
 	public Brand findByName(String name); 
 	
 	public Long countById(Integer id); 
