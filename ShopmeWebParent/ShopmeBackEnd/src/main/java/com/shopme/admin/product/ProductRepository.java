792ec5394c11134a2dc642cd774de8e0da879b29
jinjin1919 <75953443+jinjin1919@users.noreply.github.com>
2024-07-12 11:19:07 -0400

    check unique product name

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRepository.java
@@ -7,4 +7,5 @@ import com.shopme.common.entity.Product;
 
 public interface ProductRepository extends CrudRepository<Product,Integer>, PagingAndSortingRepository<Product, Integer> {
 
+	public Product findByName(String name); 
 }
